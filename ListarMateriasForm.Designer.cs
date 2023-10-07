ebe860f9dfd58dc3f8697efd1c50f1a4e6e62532
Guido <guidoolivera1@gmail.com>
2023-10-07 18:12:48 -0300

    Cambios para la funcionalidad de los roles

--- ListarMateriasForm.Designer.cs
+++ ListarMateriasForm.Designer.cs
@@ -29,37 +29,26 @@
         private void InitializeComponent()
         {
             this.dataGridViewMaterias = new System.Windows.Forms.DataGridView();
-            this.comboBoxAlumnos = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMaterias)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridViewMaterias
             // 
             this.dataGridViewMaterias.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewMaterias.Location = new System.Drawing.Point(34, 35);
+            this.dataGridViewMaterias.Location = new System.Drawing.Point(30, 40);
             this.dataGridViewMaterias.Name = "dataGridViewMaterias";
-            this.dataGridViewMaterias.Size = new System.Drawing.Size(487, 214);
+            this.dataGridViewMaterias.Size = new System.Drawing.Size(636, 316);
             this.dataGridViewMaterias.TabIndex = 0;
-            dataGridViewMaterias.CellFormatting += dataGridViewMaterias_CellFormatting;
-            // 
-            // comboBoxAlumnos
-            // 
-            this.comboBoxAlumnos.FormattingEnabled = true;
-            this.comboBoxAlumnos.Location = new System.Drawing.Point(34, 280);
-            this.comboBoxAlumnos.Name = "comboBoxAlumnos";
-            this.comboBoxAlumnos.Size = new System.Drawing.Size(487, 21);
-            this.comboBoxAlumnos.TabIndex = 1;
             // 
             // ListarMateriasForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SteelBlue;
-            this.ClientSize = new System.Drawing.Size(557, 384);
-            this.Controls.Add(this.comboBoxAlumnos);
+            this.ClientSize = new System.Drawing.Size(700, 384);
             this.Controls.Add(this.dataGridViewMaterias);
             this.Name = "ListarMateriasForm";
-            this.Text = "InscribirAMateriaForm";
+            this.Text = "ListarMateriasForm";
             this.Load += new System.EventHandler(this.ListarMateriasForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMaterias)).EndInit();
             this.ResumeLayout(false);
@@ -69,6 +58,5 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridViewMaterias;
-        private System.Windows.Forms.ComboBox comboBoxAlumnos;
     }
 }
