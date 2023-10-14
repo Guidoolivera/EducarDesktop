8a03314b3cc1c0463a7550fa4c8bf14655fa8e13
Guido <guidoolivera1@gmail.com>
2023-10-13 22:20:22 -0300

    Funcionalidades agregadas. Inscripción a materias creada.

--- ListarMateriasForm.Designer.cs
+++ ListarMateriasForm.Designer.cs
@@ -28,34 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridViewMaterias = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMaterias)).BeginInit();
+            this.dgvMaterias = new System.Windows.Forms.DataGridView();
+            this.listBoxAlumnos = new System.Windows.Forms.ListBox();
+            this.lblAlumnosInscritos = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMaterias)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridViewMaterias
+            // dgvMaterias
             // 
-            this.dataGridViewMaterias.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewMaterias.Location = new System.Drawing.Point(30, 40);
-            this.dataGridViewMaterias.Name = "dataGridViewMaterias";
-            this.dataGridViewMaterias.Size = new System.Drawing.Size(636, 316);
-            this.dataGridViewMaterias.TabIndex = 0;
+            this.dgvMaterias.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMaterias.Location = new System.Drawing.Point(23, 40);
+            this.dgvMaterias.Name = "dgvMaterias";
+            this.dgvMaterias.Size = new System.Drawing.Size(436, 186);
+            this.dgvMaterias.TabIndex = 0;
+            // 
+            // listBoxAlumnos
+            // 
+            this.listBoxAlumnos.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.listBoxAlumnos.FormattingEnabled = true;
+            this.listBoxAlumnos.Location = new System.Drawing.Point(476, 40);
+            this.listBoxAlumnos.Name = "listBoxAlumnos";
+            this.listBoxAlumnos.Size = new System.Drawing.Size(166, 186);
+            this.listBoxAlumnos.TabIndex = 1;
+            // 
+            // lblAlumnosInscritos
+            // 
+            this.lblAlumnosInscritos.AutoSize = true;
+            this.lblAlumnosInscritos.Location = new System.Drawing.Point(473, 9);
+            this.lblAlumnosInscritos.Name = "lblAlumnosInscritos";
+            this.lblAlumnosInscritos.Size = new System.Drawing.Size(91, 13);
+            this.lblAlumnosInscritos.TabIndex = 2;
+            this.lblAlumnosInscritos.Text = "Alumnos inscritos:";
             // 
             // ListarMateriasForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SteelBlue;
-            this.ClientSize = new System.Drawing.Size(700, 384);
-            this.Controls.Add(this.dataGridViewMaterias);
+            this.ClientSize = new System.Drawing.Size(654, 283);
+            this.Controls.Add(this.lblAlumnosInscritos);
+            this.Controls.Add(this.listBoxAlumnos);
+            this.Controls.Add(this.dgvMaterias);
             this.Name = "ListarMateriasForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ListarMateriasForm";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMaterias)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMaterias)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridViewMaterias;
+        private System.Windows.Forms.DataGridView dgvMaterias;
+        private System.Windows.Forms.ListBox listBoxAlumnos;
+        private System.Windows.Forms.Label lblAlumnosInscritos;
     }
 }
---
+++ SolicitudesInscripcionForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace EducarWeb
+{
+    partial class SolicitudesInscripcionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_AceptarSolicitud = new System.Windows.Forms.Button();
+            this.dgvSolicitudes = new System.Windows.Forms.DataGridView();
+            this.btn_RechazarSolicitud = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSolicitudes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_AceptarSolicitud
+            // 
+            this.btn_AceptarSolicitud.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.btn_AceptarSolicitud.Location = new System.Drawing.Point(64, 225);
+            this.btn_AceptarSolicitud.Name = "btn_AceptarSolicitud";
+            this.btn_AceptarSolicitud.Size = new System.Drawing.Size(125, 31);
+            this.btn_AceptarSolicitud.TabIndex = 3;
+            this.btn_AceptarSolicitud.Text = "ACEPTAR";
+            this.btn_AceptarSolicitud.UseVisualStyleBackColor = false;
+            this.btn_AceptarSolicitud.Click += new System.EventHandler(this.btn_AceptarSolicitud_Click);
+            // 
+            // dgvSolicitudes
+            // 
+            this.dgvSolicitudes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSolicitudes.Location = new System.Drawing.Point(12, 12);
+            this.dgvSolicitudes.Name = "dgvSolicitudes";
+            this.dgvSolicitudes.Size = new System.Drawing.Size(393, 166);
+            this.dgvSolicitudes.TabIndex = 4;
+            // 
+            // btn_RechazarSolicitud
+            // 
+            this.btn_RechazarSolicitud.BackColor = System.Drawing.Color.Firebrick;
+            this.btn_RechazarSolicitud.Location = new System.Drawing.Point(217, 225);
+            this.btn_RechazarSolicitud.Name = "btn_RechazarSolicitud";
+            this.btn_RechazarSolicitud.Size = new System.Drawing.Size(125, 31);
+            this.btn_RechazarSolicitud.TabIndex = 5;
+            this.btn_RechazarSolicitud.Text = "RECHAZAR";
+            this.btn_RechazarSolicitud.UseVisualStyleBackColor = false;
+            this.btn_RechazarSolicitud.Click += new System.EventHandler(this.btn_RechazarSolicitud_Click);
+            // 
+            // SolicitudesInscripcionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(417, 268);
+            this.Controls.Add(this.btn_RechazarSolicitud);
+            this.Controls.Add(this.dgvSolicitudes);
+            this.Controls.Add(this.btn_AceptarSolicitud);
+            this.Name = "SolicitudesInscripcionForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ListarMateriasForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSolicitudes)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_AceptarSolicitud;
+        private System.Windows.Forms.DataGridView dgvSolicitudes;
+        private System.Windows.Forms.Button btn_RechazarSolicitud;
+    }
+}
